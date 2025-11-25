9a5358daa9cde63916fc658e21928ede75e8b6a6
Mohamed Sulaiman <mohamedsulaiman876@gmail.com>
2025-11-25 12:41:04 +0530

    Update Terraform required version to 1.14.0

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.13.4" // added latest version
+    required_version = "~> 1.14.0" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
